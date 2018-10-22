f72f9d76b836713caed49fad9d5a46dc3ccc4522
Lng123 <louisng1@hotmail.com>
2018-10-21 19:51:00 -0700

    redesigned methods

--- saverecord.c
+++ saverecord.c
@@ -16,15 +16,99 @@ int saverecordmain(void) {
 
 	account *record = malloc(sizeof(account));
 	while (enter_record("Enter the website\n", "Enter the username\n", "Enter the password\n", record) != 0) {
-		removeSpaces(record);
-		store_record(record);
-		printf("success!\n");
 	}
 	free(record);
 
 	return 0;
 }
 
+int confirmation(char *username, char *password, char *site, char *confirm, account *prec){
+	char line[BUFSIZE];
+    while (1) {
+        printf("Is this correct?(Y/N)\n");
+        printf("%s %s %s\n", site, username, password);
+
+        if (!fgets(line, BUFSIZE, stdin)) {
+            clearerr(stdin);
+            return 0;
+        }
+        sscanf(line, "%[YyNn]", confirm);
+
+        if (strcmp(confirm, "Y") == 0 || strcmp(confirm, "y") == 0) {
+            encrypt(password);
+            strcpy(prec->username, username);
+            strcpy(prec->password, password);
+            strcpy(prec->site, site);
+        
+                printf("stored %s %s %s\n", prec->site, prec->username, prec->password);
+        
+                printf("stored\n");
+            
+                return 1;
+        }
+
+        if (strcmp(confirm, "N") == 0 || strcmp(confirm, "n") == 0) {
+            printf("cleared");
+            clearerr(stdin);
+            return 0;
+        }
+    }
+}
+
+int passwordvalid(const char *prompt, char *username, char *password, char *site, char *confirm, account *prec){
+	int length;
+	char line[BUFSIZE];
+	char pass[NSIZE];
+	printf("%s", prompt);
+	while (1) {
+		if (!fgets(line, BUFSIZE, stdin)) {
+			clearerr(stdin);
+			return 0;
+		}
+		if (sscanf(line, "%s", password) != 1) {
+			clearerr(stdin);
+			printf("Invalid. %s\n", prompt);
+		}
+		else {
+			/*Enter encrypt function right here.
+			And then store it into the struct.*/
+			if (password[0] == '@') {
+				return 0;
+			}
+			if (password[0] == 'r' && password[1] == 'd') {
+				length = get_valid_int("Enter a Password Length", 0, 5, 55);
+				makepw(length, pass);
+				strcpy(password, pass);
+			}
+			if(confirmation(username, password, site, confirm, prec)) {
+				removeSpaces(prec);
+				store_record(prec);
+				printf("success!\n");
+			}
+		}
+	}
+}
+
+int usernamevalid(const char *prompt, const char *prompt2, char *username, char *password, char *site, char *confirm, account *prec) {
+	char line[BUFSIZE];
+	printf("%s", prompt);
+	while (1) {
+		if (!fgets(line, BUFSIZE, stdin)) {
+			clearerr(stdin);
+			return 0;
+		}
+		if (sscanf(line, "%s", username) != 1) {
+			clearerr(stdin);
+			printf("Invalid. %s\n", prompt);
+		}
+		else {
+			if (username[0] == '@') {
+				return 0;
+			}
+			passwordvalid(prompt2, username, password, site, confirm, prec);
+		}
+	}
+}
 
 int enter_record(const char *prompt1, const char *prompt2, const char *prompt3, account *prec) {
 	char line[BUFSIZE];
@@ -32,8 +116,6 @@ int enter_record(const char *prompt1, const char *prompt2, const char *prompt3, 
 	char username[NSIZE];
 	char password[NSIZE];
 	char confirm[NSIZE];
-	char pass[NSIZE];
-	int length;
 
 	printf("%s", prompt1);
 	while (1) {
@@ -49,76 +131,7 @@ int enter_record(const char *prompt1, const char *prompt2, const char *prompt3, 
 			if (site[0] == '@') {
 				return 0;
 			}
-
-			printf("%s", prompt2);
-			while (1) {
-				if (!fgets(line, BUFSIZE, stdin)) {
-					clearerr(stdin);
-					return 0;
-				}
-				if (sscanf(line, "%s", username) != 1) {
-					clearerr(stdin);
-					printf("Invalid. %s\n", prompt1);
-				}
-				else {
-					if (username[0] == '@') {
-						return 0;
-					}
-
-					printf("%s", prompt3);
-					while (1) {
-						if (!fgets(line, BUFSIZE, stdin)) {
-							clearerr(stdin);
-							return 0;
-						}
-						if (sscanf(line, "%s", password) != 1) {
-							clearerr(stdin);
-							printf("Invalid. %s\n", prompt1);
-						}
-						else {
-							/*Enter encrypt function right here.
-							And then store it into the struct.*/
-							if (password[0] == '@') {
-								return 0;
-							}
-							if (password[0] == 'r' && password[1] == 'd') {
-								length = get_valid_int("Enter a Password Length", 0, 5, 55);
-								makepw(length, pass);
-								strcpy(password, pass);
-							}
-							while (1) {
-								printf("Is this correct?(Y/N)\n");
-								printf("%s %s %s\n", site, username, password);
-
-								if (!fgets(line, BUFSIZE, stdin)) {
-									clearerr(stdin);
-									return 0;
-								}
-								sscanf(line, "%[YyNn]", confirm);
-
-								if (strcmp(confirm, "Y") == 0 || strcmp(confirm, "y") == 0) {
-									encrypt(password);
-									strcpy(prec->username, username);
-									strcpy(prec->password, password);
-									strcpy(prec->site, site);
-								
-										printf("stored %s %s %s\n", prec->site, prec->username, prec->password);
-								
-										printf("stored\n");
-									
-										return 1;
-								}
-
-								if (strcmp(confirm, "N") == 0 || strcmp(confirm, "n") == 0) {
-									printf("cleared");
-									clearerr(stdin);
-									return 0;
-								}
-							}
-						}
-					}
-				}
-			}
+			usernamevalid(prompt2, prompt3, username, password, site, confirm, prec);
 		}
 	}
 	return 0;
